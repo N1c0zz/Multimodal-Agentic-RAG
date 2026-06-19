9f0fcb1537a8b11c1c976e04094a9f637db720f8
Nicolò Morini <nicomorini25@gmail.com>
2026-06-19 18:15:02 +0200

    feat: update run_test.sh to accept parameters by command line

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#----USAGE EXAMPLE----
+# sbatch run.sh /homes/$USER/cvcs2026/test_qwen_VL.py
+
 #SBATCH --job-name=test_qwen
 #SBATCH --partition=all_usr_prod
 #SBATCH --qos=all_qos_dbg
@@ -6,16 +10,16 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=00:30:00
-#SBATCH --output=/work/cvcs2026/dati_progetto/test_%j.out
-#SBATCH --error=/work/cvcs2026/dati_progetto/test_%j.err
+#SBATCH --output=/homes/%u/cvcs2026/logs/out/test_%j.out
+#SBATCH --error=/homes/%u/cvcs2026/logs/err/test_%j.err
 #SBATCH --account=cvcs2026
 
 export HF_HOME=/work/cvcs2026/dati_progetto/.cache_hf
 export TORCH_HOME=/work/cvcs2026/dati_progetto/.cache_torch
 
 module load python/3.11.11-gcc-11.4.0
-source /homes/nmorini/cvcs2026/venv/bin/activate
+source /homes/$USER/cvcs2026/venv/bin/activate
 
 echo "Avvio job su nodo: $SLURMD_NODENAME"
-python /homes/nmorini/cvcs2026/test_qwen.py
+python ${1}
 echo "Job terminato."
