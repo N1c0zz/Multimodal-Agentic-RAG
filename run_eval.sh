85d64b48620d2cfc9db940cac206a28ae4bb051a
Nicolò Morini <nicomorini25@gmail.com>
2026-07-13 19:12:18 +0200

    fix: update SLURM job configuration and script paths for evaluation

--- run_eval.sh
+++ run_eval.sh
@@ -3,21 +3,21 @@
 #SBATCH --partition=all_usr_prod
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus=0
+#SBATCH --gres=gpu:1
 #SBATCH --mem=20G
 #SBATCH --time=03:00:00
 #SBATCH --output=/homes/%u/cvcs2026/logs/out/evqa_eval_%j.out
 #SBATCH --error=/homes/%u/cvcs2026/logs/err/evqa_eval_%j.err
 #SBATCH --account=cvcs2026
 
-source /homes/$USER/cvcs2026/venv_eval/bin/activate
+source /work/cvcs2026/feature_extractors/venv_eval/bin/activate
 
 PRED_DIR=/work/cvcs2026/feature_extractors/dati_progetto/predictions/baseline_qwen
 
 echo "Avvio Encyclopedic-VQA eval su nodo: $SLURMD_NODENAME"
 echo "Predictions dir: ${PRED_DIR}"
 
-python /homes/$USER/cvcs2026/scripts/evqa_eval.py \
+python /homes/$USER/cvcs2026/evqa_eval/evqa_compute_metrics.py \
     --input_path ${PRED_DIR}
 
 echo "Eval terminata."
