4af45ca70a87b724f2b247c61bfbfac36bb55f87
Nicolò Morini <nicomorini25@gmail.com>
2026-07-13 17:14:48 +0200

    fix: update SLURM job configuration for inference script

--- run_inference.sh
+++ run_inference.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=qwen_inference
-#SBATCH --partition=all_usr_prod
+#SBATCH --partition=boost_usr_prod
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=03:30:00
+#SBATCH --constraint="gpu_A40_45G|gpu_L40S_45G"
+#SBATCH --time=06:00:00
+#SBATCH --mem=64G
 #SBATCH --output=/homes/%u/cvcs2026/logs/out/inference_%j.out
 #SBATCH --error=/homes/%u/cvcs2026/logs/err/inference_%j.err
 #SBATCH --account=cvcs2026
