59a9f0becb6bce63f95bbd75b66a5cd2d2550a79
Nicolò Morini <nicomorini25@gmail.com>
2026-07-28 11:33:30 +0200

    refactor: moved run_eval.sh script into evqa_eval folder

--- evqa_eval/run_eval.sh
+++ evqa_eval/run_eval.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=evqa_eval
 #SBATCH --partition=all_usr_prod
+#SBATCH --qos=all_qos_dbg
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=20G
 #SBATCH --time=03:00:00
-#SBATCH --output=/homes/%u/cvcs2026/logs/out/evqa_eval_%j.out
-#SBATCH --error=/homes/%u/cvcs2026/logs/err/evqa_eval_%j.err
+#SBATCH --output=/homes/%u/cvcs2026/logs/out/eval_agent_test_%j.out
+#SBATCH --error=/homes/%u/cvcs2026/logs/err/eval_agent_test_%j.err
 #SBATCH --account=cvcs2026
 
 source /work/cvcs2026/feature_extractors/venv_eval/bin/activate
 
-PRED_DIR=/work/cvcs2026/feature_extractors/dati_progetto/predictions/baseline_qwen
+PRED_DIR=/work/cvcs2026/feature_extractors/dati_progetto/predictions/agent_test
 
 echo "Avvio Encyclopedic-VQA eval su nodo: $SLURMD_NODENAME"
 echo "Predictions dir: ${PRED_DIR}"
